284f801a105973cc2e4c5ad10e62a88277774ec6
Ann-Britt Ström <annbritt.strom@gmail.com>
2024-12-10 20:13:05 +0100

    Added email validation

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-// import React, {useState} from 'react'
+import React, {useState} from 'react'
 
 const EmailForm = () => {
+   const [emailData, setEmailData] = useState('')
+   const [error, setError] = useState('')
+   const [submitted, setSubmitted] = useState(false)
+
+   const handleChange = (e) => {
+      const value = e.target.value
+      setEmailData(value)
+   }
+
+   const handleSubmit = (e) => {
+      e.preventDefault()
+
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      
+      if (!emailRegex.test(emailData)) {
+         setError('Enter a valid email.')
+      } else {
+         setError('')
+         alert('Thanks for subscribing.')
+      }
+
+   }
 
    return (
-   <form className="subscribe-area" >
-      <div className="input-group">
-         <input className="form-input" name='email' placeholder="✉   Your Email" id="newsletter" />
-         <button type='submit' id="submit-btn">Subscribe</button>
-      </div>
+      <form onSubmit={handleSubmit} className="subscribe-area" >
+         <div className="input-group">
+            <input className="form-input" name='email' onChange={handleChange} placeholder="✉   Your Email" id="newsletter" />
+            <button type='submit' id="submit-btn">Subscribe</button>
+         </div>
 
-      <span></span>
+         <span>{error}</span>
 
-   </form>
-  )
+      </form>
+   )
 }
 
 export default EmailForm
